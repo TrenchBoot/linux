7107b8bd58203f932e5295da413178d7cca7b120
Ross Philipson <ross.philipson@oracle.com>
2018-10-26 15:53:24 -0400

    [patch] x86: Secure Launch kernel early boot stub
    The Secure Launch (SL) stub provides the entry point for Intel TXT (and
    later AMD SKINIT) to vector to during the late launch. The symbol
    sl_stub_entry is that entry point and is conveyed launching code using
    the MLE (Measured Launch Environment) header represented by the symbol
    sl_mle_header. The offset of the MLE is written to the boot params at
    build time. The routine sl_stub contains the very early late launch setup
    code responsible for setting up the basic environment to allow the normal
    kernel startup_32 code to proceed. It is also responsible for properly
    waking and handling the APs on Intel platforms. The routine sl_main which
    runs after entering 64b mode is responsible for measuring configuration
    information before it is used like the boot params, the kernel command
    line, the TXT heap, etc.
    
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>
    Signed-off-by: Eric Snowberg <eric.snowberg@oracle.com>
    Signed-off-by: Daniel P. Smith <dpsmith@apertussolutions.com>

--- arch/x86/kernel/asm-offsets.c
+++ arch/x86/kernel/asm-offsets.c
@@ -12,6 +12,7 @@
 #include <linux/hardirq.h>
 #include <linux/suspend.h>
 #include <linux/kbuild.h>
+#include <linux/slaunch.h>
 #include <asm/processor.h>
 #include <asm/thread_info.h>
 #include <asm/sigframe.h>
@@ -105,4 +106,15 @@ static void __used common(void)
 	OFFSET(TSS_sp0, tss_struct, x86_tss.sp0);
 	OFFSET(TSS_sp1, tss_struct, x86_tss.sp1);
 	OFFSET(TSS_sp2, tss_struct, x86_tss.sp2);
+
+#ifdef CONFIG_SECURE_LAUNCH
+	BLANK();
+	OFFSET(SL_zero_page_addr, txt_os_mle_data, zero_page_addr);
+	OFFSET(SL_saved_misc_enable_msr, txt_os_mle_data, saved_misc_enable_msr);
+	OFFSET(SL_saved_bsp_mtrrs, txt_os_mle_data, saved_bsp_mtrrs);
+	OFFSET(SL_ap_wake_ebp, txt_os_mle_data, ap_wake_ebp);
+	OFFSET(SL_ap_wake_block, txt_os_mle_data, ap_wake_block);
+	OFFSET(SL_ap_gdt_base, txt_mle_join, ap_gdt_base);
+	OFFSET(SL_ap_entry_point, txt_mle_join, ap_entry_point);
+#endif
 }
