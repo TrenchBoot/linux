45fa925c646fd79934750a08bcfd5fba2d78956d
Daniel P. Smith <dpsmith@apertussolutions.com>
2019-11-18 16:53:19 -0500

    revision: for {e8bf599d3613}
    code review for early_sha1 changes
    
    Signed-off-by: Daniel P. Smith <dpsmith@apertussolutions.com>

--- arch/x86/boot/compressed/early_sha1.h
+++ arch/x86/boot/compressed/early_sha1.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _ASM_X86_SHA1_H
-#define _ASM_X86_SHA1_H
+#ifndef BOOT_COMPRESSED_EARLY_SHA1_H
+#define BOOT_COMPRESSED_EARLY_SHA1_H
 
 /*
  * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
@@ -14,4 +14,4 @@ void early_sha1_update(struct sha1_state *sctx,
 		       unsigned int len);
 void early_sha1_final(struct sha1_state *sctx, u8 *out);
 
-#endif /* _ASM_X86_SHA1_H */
+#endif /* BOOT_COMPRESSED_EARLY_SHA1_H */
