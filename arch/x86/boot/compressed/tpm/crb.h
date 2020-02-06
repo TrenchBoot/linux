e89401f98623a0de1553b41c8069658636dbc8a4
Daniel P. Smith <dpsmith@apertussolutions.com>
2020-02-06 12:59:09 -0500

    x86: Add early TPM TIS/CRB interface support for Secure Launch
    The Secure Launch capability that is part of the compressed kernel
    requires the ability to send measurements it takes to the TPM. This
    commit introduces the necessary code to communicate with the hardware
    interface of TPM devices.
    
    Signed-off-by: Daniel P. Smith <dpsmith@apertussolutions.com>

---
+++ arch/x86/boot/compressed/tpm/crb.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 Apertus Solutions, LLC
+ *
+ * Author(s):
+ *      Daniel P. Smith <dpsmith@apertussolutions.com>
+ *
+ * The definitions in this header are extracted from the Trusted Computing
+ * Group's "TPM Main Specification", Parts 1-3.
+ *
+ */
+
+#ifndef _CRB_H
+#define _CRB_H
+
+#include "tpm.h"
+
+/* TPM Interface Specification functions */
+u8 crb_request_locality(u8 l);
+void crb_relinquish_locality(void);
+u8 crb_init(struct tpm *t);
+size_t crb_send(struct tpmbuff *buf);
+size_t crb_recv(struct tpmbuff *buf);
+
+#endif
