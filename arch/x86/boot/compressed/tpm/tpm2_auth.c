f326a838319dd76255943b8a7008680382a8af6b
Daniel P. Smith <dpsmith@apertussolutions.com>
2020-02-06 14:29:17 -0500

    x86: Add early TPM1.2/TPM2.0 interface support for Secure Launch
    This commit introduces an abstraction for TPM1.2 and TPM2.0 devices
    above the TPM hardware interface.
    
    Signed-off-by: Daniel P. Smith <dpsmith@apertussolutions.com>

---
+++ arch/x86/boot/compressed/tpm/tpm2_auth.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 Apertus Solutions, LLC
+ *
+ * Author(s):
+ *      Daniel P. Smith <dpsmith@apertussolutions.com>
+ *
+ */
+
+#include <linux/types.h>
+#include <linux/string.h>
+#include <asm/byteorder.h>
+#include "tpm.h"
+#include "tpm2.h"
+#include "tpm2_constants.h"
+
+#define NULL_AUTH_SIZE 9
+
+u16 tpm2_null_auth_size(void)
+{
+	return NULL_AUTH_SIZE;
+}
+
+u16 tpm2_null_auth(struct tpms_auth_cmd *a)
+{
+	memset(a, 0, NULL_AUTH_SIZE);
+
+	*a->handle = cpu_to_be32(TPM_RS_PW);
+
+	return NULL_AUTH_SIZE;
+}
