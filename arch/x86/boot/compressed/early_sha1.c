7c7d53e5fb8051eac6589d3f984a245e143f3b8f
Ross Philipson <ross.philipson@oracle.com>
2020-08-31 13:37:01 -0400

    [revision] for {b7e9ed7f1753047791ba42a0442ac48f9b577a65}
    Fix copyright statements.
    
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- arch/x86/boot/compressed/early_sha1.c
+++ arch/x86/boot/compressed/early_sha1.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, Oracle and/or its affiliates.
  * Copyright (c) 2019 Apertus Solutions, LLC.
  */
 
--- arch/x86/boot/compressed/early_sha1.h
+++ arch/x86/boot/compressed/early_sha1.h
@@ -1,10 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates.
+ */
+
 #ifndef BOOT_COMPRESSED_EARLY_SHA1_H
 #define BOOT_COMPRESSED_EARLY_SHA1_H
-
-/*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
- */
 
 #include <crypto/sha.h>
 
